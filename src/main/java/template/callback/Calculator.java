1a7a891b9d2dcff1c256783389d60c6866d0ba10
Sumin Hong <dufmaakedl@gmail.com>
2020-09-17 20:49:47 +0900

    upgrade code with Try-Catch

--- src/main/java/template/callback/Calculator.java
+++ src/main/java/template/callback/Calculator.java
@@ -7,23 +7,47 @@ import java.io.IOException;
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filepath));
-
-        int sum=0;
-        String line;
-        while((line=br.readLine())!= null){
-            sum += Integer.parseInt(line);
+        try{
+            int sum = 0;
+            String line;
+            while((line=br.readLine())!= null){
+                sum += Integer.parseInt(line);
+            }
+            return sum;
+        }catch(IOException exception){
+            System.out.println(exception.getMessage());
+            throw exception;
+        }finally{
+            if(br!=null){
+                try{
+                    br.close();
+                }catch(IOException exception){
+                    System.out.println(exception.getMessage());
+                }
+            }
         }
-        return sum;
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filepath));
-
-        int sum=1;
-        String line;
-        while((line=br.readLine())!= null){
-            sum *= Integer.parseInt(line);
+        try{
+            int ret = 1;
+            String line;
+            while((line=br.readLine())!= null){
+                ret *= Integer.parseInt(line);
+            }
+            return ret;
+        }catch(IOException exception){
+            System.out.println(exception.getMessage());
+            throw exception;
+        }finally{
+            if(br!=null){
+                try{
+                    br.close();
+                }catch(IOException exception){
+                    System.out.println(exception.getMessage());
+                }
+            }
         }
-        return sum;
     }
 }

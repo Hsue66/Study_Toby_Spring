18a4895808cfd914c6b57474ea4d649ca3d4d376
Sumin Hong <dufmaakedl@gmail.com>
2020-09-21 22:08:39 +0900

    apply Spring

--- src/main/java/spring/user/domain/DaoFactory.java
+++ src/main/java/spring/user/domain/DaoFactory.java
@@ -1,14 +1,21 @@
 package spring.user.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao(){
         return new UserDao(new kCon());
     }
 
+    @Bean
     public AccountDao accountDao(){
         return new AccountDao(new kCon());
     }
 
+    @Bean
     public MessageDao messageDao(){
         return new MessageDao(new kCon());
     }
--- src/main/java/spring/user/domain/Main.java
+++ src/main/java/spring/user/domain/Main.java
@@ -1,10 +1,14 @@
 package spring.user.domain;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao",UserDao.class);
 
         User user = new User();
         user.setId("hsue");

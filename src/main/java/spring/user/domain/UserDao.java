202bfd4c4e0541be9a294d0aaee28ab61c14be42
Sumin Hong <dufmaakedl@gmail.com>
2020-09-21 21:20:12 +0900

    Refactoring : extract common part as a method

--- src/main/java/spring/user/domain/UserDao.java
+++ src/main/java/spring/user/domain/UserDao.java
@@ -4,27 +4,24 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?serverTimezone=UTC","root","1234");
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement("INSERT INTO users (id,name,password) VALUES (?,?,?)");
+        ps.setString(1,user.getId());
+        ps.setString(2,user.getName());
+        ps.setString(3,user.getPassword());
 
-        PreparedStatement stmt = c.prepareStatement("INSERT INTO users (id,name,password) VALUES (?,?,?)");
-        stmt.setString(1,user.getId());
-        stmt.setString(2,user.getName());
-        stmt.setString(3,user.getPassword());
+        ps.executeUpdate();
 
-        stmt.executeUpdate();
-
+        ps.close();
         c.close();
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?serverTimezone=UTC","root","1234");
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement("SELECT * FROM users WHERE id=?");
+        ps.setString(1,id);
 
-        PreparedStatement stmt = c.prepareStatement("SELECT * FROM users WHERE id=?");
-        stmt.setString(1,id);
-
-        ResultSet rs = stmt.executeQuery();
+        ResultSet rs = ps.executeQuery();
 
         rs.next();
         User user = new User();
@@ -32,8 +29,16 @@ public class UserDao {
         user.setName(rs.getString(2));
         user.setPassword(rs.getString(3));
 
+        ps.close();
         rs.close();
         c.close();
+
         return user;
+    }
+
+    Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?serverTimezone=UTC","root","1234");
+        return c;
     }
 }

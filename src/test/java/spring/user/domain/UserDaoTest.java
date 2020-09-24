f4d27a1f7e81e806d38a76bb36ae4cc1bc5a20e5
Sumin Hong <dufmaakedl@gmail.com>
2020-09-24 23:15:36 +0900

    RunWith spring jUnit

--- src/test/java/spring/user/domain/UserDaoTest.java
+++ src/test/java/spring/user/domain/UserDaoTest.java
@@ -2,24 +2,32 @@ package spring.user.domain;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
     UserDao userDao;
     User user;
 
     @Before
     public void setup(){
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml"); //new AnnotationConfigApplicationContext(DaoFactory.class);
         this.userDao = context.getBean("userDao",UserDao.class);
         this.user = new User("hsue","Sumin Hong","66");
     }

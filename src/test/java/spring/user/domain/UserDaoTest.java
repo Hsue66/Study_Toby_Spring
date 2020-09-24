650004161476666389f062aa2dc7d84005cccdf5
Sumin Hong <dufmaakedl@gmail.com>
2020-09-24 23:11:01 +0900

    change bean to xml file

--- src/test/java/spring/user/domain/UserDaoTest.java
+++ src/test/java/spring/user/domain/UserDaoTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
@@ -17,13 +19,13 @@ public class UserDaoTest {
 
     @Before
     public void setup(){
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml"); //new AnnotationConfigApplicationContext(DaoFactory.class);
         this.userDao = context.getBean("userDao",UserDao.class);
         this.user = new User("hsue","Sumin Hong","66");
     }
 
     @Test
-    public void addAndGetTest() throws SQLException, ClassNotFoundException {
+    public void addAndGetTest() throws SQLException {
         userDao.delete();
         assertThat(userDao.count(), is(0));
 
